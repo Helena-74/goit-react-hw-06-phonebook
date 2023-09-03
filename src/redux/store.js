c453e25d93c0e68c13a2a3a7119187b61b4f63e8
Helena-74 <122282363+Helena-74@users.noreply.github.com>
2023-09-03 13:20:47 +0200

    commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,18 @@ import { configureStore, createSlice } from '@reduxjs/toolkit';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-const contactsSlice = createSlice({
+const contactsInitialState = {
+  items: [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ],
+};
+
+export const contactsSlice = createSlice({
   name: 'contacts',
-  initialState: [],
+  initialState: contactsInitialState,
   reducers: {
     deleteContact: (state, action) => {
       state = state.filter(el => el.id !== action.payload)
