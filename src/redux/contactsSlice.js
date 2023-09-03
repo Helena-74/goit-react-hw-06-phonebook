0cc75c642cf11b764c62381d31d40f4aa09223a3
Helena-74 <122282363+Helena-74@users.noreply.github.com>
2023-09-03 14:54:02 +0200

    commit

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,45 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+// import { nanoid } from 'nanoid';
+import { persistReducer } from 'redux-persist';
+import persistStore from 'redux-persist/es/persistStore';
+import storage from 'redux-persist/lib/storage';
+
+const contactsInitialState = {
+  items: [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ],
+};
+
+export const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: contactsInitialState,
+  reducers: {
+    deleteContact: (state, action) => {
+      state = state.filter(el => el.id !== action.payload)
+
+    },
+    addContact: (state, action) => {
+      state = [...state, action.payload]
+    },
+  }, 
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedContactsSlice = persistReducer(persistConfig, contactsSlice.reducer);
+
+export const { deleteContact, addContact } = contactsSlice.actions;
+
+export const store = configureStore({
+  reducer: {
+    contacts: persistedContactsSlice,
+  },
+});
+
+export const persistor = persistStore(store);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,46 +1,18 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
-import { persistReducer, persistStore } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
-const contactsInitialState = {
-  items: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
-};
-
-export const contactsSlice = createSlice({
-  name: 'contacts',
-  initialState: contactsInitialState,
-  reducers: {
-    deleteContact: (state, action) => {
-      state = state.filter(el => el.id !== action.payload)
-
-    },
-    addContact: (state, action) => {
-      state = [...state, action.payload]
-    },
-  }, 
-});
-
-const persistConfig = {
-  key: 'root',
-  storage,
-};
-
-const persistedContactsSlice = persistReducer(persistConfig, contactsSlice.reducer);
-
-export const { deleteContact, addContact } = contactsSlice.actions;
+// import { configureStore, createSlice } from '@reduxjs/toolkit';
+// import { persistReducer, persistStore } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+import { configureStore } from '@reduxjs/toolkit';
+import { persistedReducer } from './contactsSlice';
+import { filterReducer } from './filterSlice';
 
 export const store = configureStore({
   reducer: {
-    contacts: persistedContactsSlice,
+    contacts: persistedReducer,
+    filter: filterReducer,
   },
-});
 
-export const persistor = persistStore(store);
+
+});
 
 
 
