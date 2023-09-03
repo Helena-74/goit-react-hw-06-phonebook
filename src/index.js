db8bca324aace3eed5082a7a712aa00147b4fccc
Helena-74 <122282363+Helena-74@users.noreply.github.com>
2023-09-03 16:05:52 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -17,8 +17,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   ,
 );
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+
+
--- src/redux/store.js
+++ src/redux/store.js
@@ -5,18 +5,26 @@ import { configureStore } from '@reduxjs/toolkit';
 import { persistedReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
 import persistStore from 'redux-persist/es/persistStore';
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 
 export const store = configureStore({
   reducer: {
     contacts: persistedReducer,
     filter: filterReducer,
   },
-
+  middleware: getDefaultMiddleware =>
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 
 });
 
 
 export const persistor = persistStore(store);
+
+
 
 // import { configureStore, createSlice } from '@reduxjs/toolkit';
 // import { persistReducer, persistStore } from 'redux-persist';
